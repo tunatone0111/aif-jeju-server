3aeb0057bd6258159770d9bb1495ff3b01c06423
Junha Park <junha.park@yonsei.ac.kr>
2024-01-20 15:58:49 +0900

    fix

--- src/trash/schema/trash.schema.ts
+++ src/trash/schema/trash.schema.ts
@@ -1,12 +1,11 @@
 import { Prop, Schema, SchemaFactory, raw } from '@nestjs/mongoose';
-import { HydratedDocument, SchemaTypes, Types } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 import { TrashType } from './trashType.enum';
 
 export type TrashDocument = HydratedDocument<Trash>;
 
 @Schema()
 export class Trash {
-  @Prop({ type: SchemaTypes.ObjectId })
   _id: Types.ObjectId;
 
   @Prop({ type: Number })
